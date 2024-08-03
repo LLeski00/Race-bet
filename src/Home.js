deedc38861e7008af3b6b2687c972ac845a791c4
Luka <llesko00@fesb.hr>
2024-08-03 13:08:01 +0200

    First commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="home-content">
+                <h1>Hello</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
