496c5412c1e487e1e1bedd4d998e892f1bb75757
mmaras02 <mmaras02@fesb.hr>
2024-08-04 11:22:48 +0200

    Moving cars

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,31 @@
-import { useEffect } from "react";
 import "./css/home.css";
 
 const Home = () => {
-    useEffect(() => {
-        startRace();
-    }, []);
 
     const startRace = () => {
-        let car = document.getElementsByClassName("car");
+        let cars = document.getElementsByClassName("car");
+        for(let i = 0; i < cars.length; i++) {
+            console.log(cars.length);
+            cars[i].children[0].style.animation= "moveRight 2s linear forwards";
+        }
     };
 
     return (
         <div className="Home">
             <div className="home-content">
                 <h1>Hello</h1>
-                <div className="car">
-                    <img id="car1" src="Icon.png" alt="car" />
+                <div className="cars">
+                    <div className="car">
+                        <img id="car1" src="./images/RacerCar.png" alt="car" />
+                    </div>
+                    <div className="car">
+                        <img id="car2" src="./images/RacerCar.png" alt="car" />
+                    </div>
+                    <div className="car">
+                        <img id="car3" src="./images/RacerCar.png" alt="car" />
+                    </div>
                 </div>
+                <button onClick={()=>startRace()}>Start race</button>
             </div>
         </div>
     );
