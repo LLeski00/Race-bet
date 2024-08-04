0c60642f9c379291c187f881b7446dbbbbeeb62b
Luka <llesko00@fesb.hr>
2024-08-04 09:41:35 +0200

    Nothing

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+import "./css/home.css";
+
 const Home = () => {
+    useEffect(() => {
+        startRace();
+    }, []);
+
+    const startRace = () => {
+        let car = document.getElementsByClassName("car");
+    };
+
     return (
         <div className="Home">
             <div className="home-content">
                 <h1>Hello</h1>
+                <div className="car">
+                    <img id="car1" src="Icon.png" alt="car" />
+                </div>
             </div>
         </div>
     );
