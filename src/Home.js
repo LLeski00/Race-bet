a1e2b4238a11f5c2947d17de195f37bd0b55be19
Luka <llesko00@fesb.hr>
2024-08-05 15:31:33 +0200

    Race functionality

--- src/Home.js
+++ src/Home.js
@@ -2,35 +2,63 @@ import { useEffect, useState } from "react";
 import "./css/home.css";
 
 const Home = () => {
-    const numOfChanges = 5;
-    let numOfCarsFinished = 0;
+    const numOfChanges = 0;
     const numOfCars = 3;
+    const maxLengthOfRace = 5;
+    const minLengthOfRace = 3;
     const lengthOfSection = 75 / (1 + numOfChanges);
-    const maxTime = 5 / (1 + numOfChanges);
-    const minTime = 3 / (1 + numOfChanges);
-    const [carTime, setCarTime] = useState(Array(numOfCars).fill(0));
+    const maxTimeOfSection = maxLengthOfRace / (1 + numOfChanges);
+    const minTimeOfSection = minLengthOfRace / (1 + numOfChanges);
+    let numOfCarsFinished = 0;
+    const [cars, setCars] = useState([]);
+    const [winner, setWinner] = useState();
 
-    const checkWinner = () => {};
+    useEffect(() => {
+        const temp = Array(numOfCars)
+            .fill()
+            .map((_, index) => ({
+                id: index + 1,
+                time: 0,
+            }));
+        setCars(temp);
+    }, []);
+
+    const checkWinner = () => {
+        setTimeout(() => {
+            let temp = maxLengthOfRace;
+            for (let i = 0; i < numOfCars; i++) {
+                if (cars[i].time < temp) {
+                    temp = cars[i].time;
+                    setWinner(cars[i].id);
+                }
+            }
+        }, maxTimeOfSection * 1000);
+    };
+
+    const updateCar = (car) => {
+        let temp = [...cars];
+        temp[car.id - 1].time = car.time;
+        setCars(temp);
+    };
 
     const setSpeed = (car, iteration) => {
-        let time = Math.random() * (maxTime - minTime) + minTime;
+        let time =
+            Math.random() * (maxTimeOfSection - minTimeOfSection) +
+            minTimeOfSection;
         if (iteration === 0) car.time = time;
-        car.time += time;
+        else car.time += time;
         car.style.transition = `left ${time}s linear`;
         car.style.left = lengthOfSection * (iteration + 1) + "vw";
-        console.log(car.style.left);
 
         if (iteration < numOfChanges) {
             setTimeout(() => {
                 setSpeed(car, iteration + 1);
             }, time * 1000);
-        } else if (numOfCarsFinished + 1 === numOfCars) {
-            setCarTime([...carTime, { id: car.id, time: car.time }]);
-            console.log(car.id + ": " + car.time);
         } else {
             numOfCarsFinished++;
+            updateCar(car);
             console.log(car.id + ": " + car.time);
-            setCarTime([...carTime, { id: car.id, time: car.time }]);
+            if (numOfCarsFinished === numOfCars) checkWinner();
         }
     };
 
@@ -45,16 +73,18 @@ const Home = () => {
         <div className="Home">
             <div className="home-content">
                 <div className="cars">
-                    <div className="car">
-                        <img id="car1" src="./images/RacerCar.png" alt="car" />
-                    </div>
-                    <div className="car">
-                        <img id="car2" src="./images/RacerCar.png" alt="car" />
-                    </div>
-                    <div className="car">
-                        <img id="car3" src="./images/RacerCar.png" alt="car" />
-                    </div>
+                    {cars &&
+                        cars.map((car) => (
+                            <div key={car.id} className="car">
+                                <img
+                                    id={car.id}
+                                    src="./images/RacerCar.png"
+                                    alt="car"
+                                ></img>
+                            </div>
+                        ))}
                 </div>
+                {winner && <p>Car number {winner} is first!</p>}
                 <button onClick={() => startRace()}>Start race</button>
             </div>
         </div>
